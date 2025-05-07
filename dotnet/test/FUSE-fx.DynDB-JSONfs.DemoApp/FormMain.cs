b2cd544001b7a701a692bb99061ea437e3eedbd5
KornSW <email@tobiaskorn.de>
2025-05-07 15:45:24 +0200

    package update

--- dotnet/test/FUSE-fx.DynDB-JSONfs.DemoApp/FormMain.cs
+++ dotnet/test/FUSE-fx.DynDB-JSONfs.DemoApp/FormMain.cs
@@ -18,6 +18,10 @@ namespace System.Data.Fuse.WinFormsDemo {
       this.InitializeComponent();
     }
 
+
+    // ACHTUNG: ÄNDERUNGEN BITTE AUCH IMMER IN DER DEMO APP DES
+    // GESCHWISTER-PROJEKTs IN 'FUSE-fx.WinForms' SYNCHRON HALTEN
+
     private void FormMain_FormClosing(object sender, FormClosingEventArgs e) {
     }
 
